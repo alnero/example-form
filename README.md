b73f101c33d24d417f4e4e87a62e21cc5c1f3da0
alnero <darestream@gmail.com>
2017-07-28 17:29:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# example-form
+Simple submit form: html, css, koa
+
+run npm install & npm start
+go to http://localhost:3000/
---
+++ form.js
@@ -0,0 +1,50 @@
+let fs = require('fs')
+
+let Koa = require("koa")
+let logger = require("koa-logger")
+let KoaRouter = require("koa-router")
+let koaStatic = require("koa-static")
+let koaBody = require("koa-body")
+
+let file = "db.json"
+
+let app = new Koa()
+
+let router = new KoaRouter()
+
+app.use(logger())
+app.use(koaStatic("./public"))
+app.use(koaBody())
+
+
+router.get("/", async (ctx, next) => {
+  ctx.response.redirect("form.html")
+  await next()
+})
+
+router.post("/", async (ctx, next) => {
+  let formData = ctx.request.body
+  let fileData = await new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if(err) reject(err)
+      resolve(data)
+    })
+  })
+
+  let db = JSON.parse(fileData)
+  db.push(formData)
+
+  fs.writeFile(file, JSON.stringify(db), (err) => {
+    if(err) console.error(err)
+  })
+
+  ctx.response.body = db
+
+  await next()
+})
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000")
+})
